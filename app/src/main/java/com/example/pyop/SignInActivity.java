726c9f9ee3c4e572b78f45cf9b5143ee2aae14a7
parth <parthdhamija101998@gmail.com>
2022-10-24 04:17:37 -0700

    Finally Pushing the Data

--- app/src/main/java/com/example/pyop/SignInActivity.java
+++ app/src/main/java/com/example/pyop/SignInActivity.java
@@ -2,6 +2,7 @@ package com.example.pyop;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,8 +15,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -26,6 +34,7 @@ public class SignInActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @Override
@@ -49,6 +58,35 @@ public class SignInActivity extends AppCompatActivity {
         if (account!=null){
             String Name = account.getDisplayName();
             String Mail = account.getEmail();
+            String LName = account.getFamilyName();
+            String Email = account.getEmail();
+            String uID = account.getId();
+
+
+
+            // Create a new user with a first and last name
+            Map<String, Object> user = new HashMap<>();
+            user.put("firstName", Name);
+            user.put("lastName", LName);
+            user.put("emailId", Email);
+            user.put("userID",uID);
+
+
+            // Add a new document with a generated ID
+            db.collection("users")
+                    .add(user)
+                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w("TAG", "Error adding document", e);
+                        }
+                    });
 
 
             name.setText(Name);
